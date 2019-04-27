51c43dff020eee7776fcff9495f39f02faf480ec
Sephirossss <benj.lemaitre@gmail.com>
2019-04-27 17:41:23 +0200

    Gitignore + correctif et capacity Wallet

--- Assets/Scripts/Wallet/WalletModule.cs
+++ Assets/Scripts/Wallet/WalletModule.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class WalletModule
 {
-
+    public int pieceCapacity, billetCapacity;
     // Start is called before the first frame update
     public WalletModule(WalletModuleSO so, Vector3 position)
     {
         MonoBehaviour.Instantiate(so.prefab, position, Quaternion.identity);
+        pieceCapacity = Random.Range(so.pieceCapacityMin, so.pieceCapacityMax);
+        billetCapacity = Random.Range(so.billetCapacityMin, so.billetCapacityMax);
+        if(pieceCapacity + billetCapacity < 1)
+        {
+            pieceCapacity = 1;
+        }
     }
 
 
