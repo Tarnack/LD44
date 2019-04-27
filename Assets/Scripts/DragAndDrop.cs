483094682d146f40c7f85aed8ed8e4b27a715aef
Zenalt <50031333+Zenalt@users.noreply.github.com>
2019-04-27 11:14:48 +0200

    Drag and Drop fixed

--- Assets/Scripts/DragAndDrop.cs
+++ Assets/Scripts/DragAndDrop.cs
@@ -4,26 +4,33 @@ using UnityEngine;
 
 public class DragAndDrop : MonoBehaviour
 {
-    private bool selected;
+ 
+    private Vector3 mOffset;
+    private float mZCoord;
+
     // Update is called once per frame
-    void Update()
+
+    private void OnMouseDrag()
     {
-      if(selected)
-            transform.position = new Vector2();
-        }
-
-        if (Input.GetMouseButtonUp(0))
-        {
-            selected = false;
-        }
-
+        transform.position = GetMouseWorldPos() + mOffset;
     }
 
-    private void OnMouseOver()
+    private void OnMouseDown()
     {
-        if(Input.GetMouseButtonDown(0))
-        {
-            selected = true;
-        }
+        mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+
+        mOffset = gameObject.transform.position - GetMouseWorldPos();
+    }
+
+    private Vector3 GetMouseWorldPos()
+    {
+        //pixel coordinates (x,y)
+        Vector3 mousePoint = Input.mousePosition;
+
+        // z coordinate of game object on screen
+        mousePoint.z = mZCoord;
+
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+
     }
 }
