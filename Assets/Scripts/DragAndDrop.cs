02bd979b1291923c4edea40b1545102b13ee2ae6
Zenalt <50031333+Zenalt@users.noreply.github.com>
2019-04-27 10:40:32 +0200

    Drag and Drop added

---
+++ Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+    private bool selected;
+    // Update is called once per frame
+    void Update()
+    {
+      if(selected)
+            transform.position = new Vector2();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            selected = false;
+        }
+
+    }
+
+    private void OnMouseOver()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            selected = true;
+        }
+    }
+}
