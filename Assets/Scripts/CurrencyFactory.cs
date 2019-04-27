601fc502cc10b781d4c13650c48a47d7cf240805
Sephirossss <benj.lemaitre@gmail.com>
2019-04-27 13:46:08 +0200

    Define Currency

---
+++ Assets/Scripts/CurrencyFactory.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurrencyFactory : MonoBehaviour
+{
+    public GameObject prefabPiece;
+    public GameObject prefabBillet;
+    public CurrencySO scriptableObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        newCurrency(scriptableObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void newCurrency(CurrencySO so)
+    {
+        GameObject prefab;
+        switch (so.type)
+        {
+            case CurrencySO.CurrencyType.Piece:
+                prefab = prefabPiece;
+                break;
+            case CurrencySO.CurrencyType.Billet:
+                prefab = prefabBillet;
+                break;
+        }
+
+        GameObject newCurrency = Instantiate(prefabPiece, Vector3.zero, Quaternion.identity);
+        newCurrency.transform.localScale = new Vector3(so.taille, so.taille, newCurrency.transform.localScale.z);
+    }
+
+}
---
+++ Assets/Scripts/CurrencySO.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New currency", menuName ="Currency")]
+public class CurrencySO: ScriptableObject
+{
+    public float value;
+    public float taille;
+    public CurrencyType type;
+
+    public enum CurrencyType
+    {
+        Piece, Billet
+    }
+}
