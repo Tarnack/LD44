7674706fe575abf890a312ac87813073d17141d2
Sephirossss <benj.lemaitre@gmail.com>
2019-04-28 16:28:54 +0200

    j'ai avancé

---
+++ Assets/Scripts/Wallet/Contenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Contenu : MonoBehaviour
+{
+    private Dictionary<CurrencySO, int> dic = new Dictionary<CurrencySO, int>();
+    
+
+    public float getValue()
+    {
+        float somme = 0;
+        foreach (CurrencySO curren in dic.Keys)
+            somme += curren.value * ;
+        return somme;
+    }
+
+
+
+}
