f6f5a9f47de163c594d3529f7f7b2db0ee27a547
Sephirossss <benj.lemaitre@gmail.com>
2019-04-29 14:14:02 +0200

    Ajout d'un gestionnaire d'event

---
+++ Assets/Scripts/Event/EventGestionnaire.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventGestionnaire : MonoBehaviour
+{
+
+    public EventSO[] listEvent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.AddComponent<AudioSource>();
+
+        startNewEvent();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void startNewEvent()
+    {
+        int numEvent = Random.Range(0, listEvent.Length);
+        EventSO newEvent = listEvent[numEvent];
+        gameObject.GetComponent<AudioSource>().clip = newEvent.soundTrack;
+    }
+}
---
+++ Assets/Scripts/Owner/Owner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Owner : MonoBehaviour
+{
+    private float memory;
+    public float frustration;
+    // Start is called before the first frame update
+    void Start()
+    {
+        frustration = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
