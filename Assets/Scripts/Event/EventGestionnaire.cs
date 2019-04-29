eab5f37e841f9eee56de502861ab407336fd8b5e
Zenalt <50031333+Zenalt@users.noreply.github.com>
2019-04-29 21:14:19 +0200

    Update EventGestionnaire.cs

--- Assets/Scripts/Event/EventGestionnaire.cs
+++ Assets/Scripts/Event/EventGestionnaire.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EventGestionnaire : MonoBehaviour
 {
 
     public EventSO[] listEvent;
+    public Text desc;
+    public Slider frustration;
+    public Image background;
     // Start is called before the first frame update
     void Start()
     {
         gameObject.AddComponent<AudioSource>();
-        
+        desc = GameObject.FindGameObjectWithTag("EventDesc").GetComponent<Text>();
+    
+
     }
 
     // Update is called once per frame
@@ -28,5 +34,11 @@ public class EventGestionnaire : MonoBehaviour
         gameObject.GetComponent<AudioSource>().clip = newEvent.soundTrack;
         gameObject.GetComponent<AudioSource>().loop = true;
         gameObject.GetComponent<AudioSource>().Play();
+        desc.text = newEvent.description;
+        if(true)
+        frustration.value += newEvent.frustAugment /100;
+
+        if(false)
+        frustration.value -= newEvent.frustBaisse /100;
     }
 }
